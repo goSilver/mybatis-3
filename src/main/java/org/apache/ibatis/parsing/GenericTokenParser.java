2cd12c2244b251d5e351ee25215a99c266beeceb
chenshaohua <silverchen961@foxmail.com>
2020-08-09 10:25:13 +0800

    tokenParser

--- src/main/java/org/apache/ibatis/parsing/GenericTokenParser.java
+++ src/main/java/org/apache/ibatis/parsing/GenericTokenParser.java
@@ -1,27 +1,38 @@
 /**
- *    Copyright 2009-2019 the original author or authors.
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
+ * Copyright 2009-2019 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.apache.ibatis.parsing;
 
 /**
+ * 占位符解析器
+ *
  * @author Clinton Begin
  */
 public class GenericTokenParser {
 
+  /**
+   * 占位符的开始标记
+   */
   private final String openToken;
+  /**
+   * 占位符的结束标记
+   */
   private final String closeToken;
+  /**
+   * TokenHandler接口的实现会按照一定的逻辑解析占位符
+   */
   private final TokenHandler handler;
 
   public GenericTokenParser(String openToken, String closeToken, TokenHandler handler) {
@@ -30,18 +41,29 @@ public class GenericTokenParser {
     this.handler = handler;
   }
 
+  /**
+   * 顺序查找openToken和closeToken，解析得到占位符的字面值，并将其交给TokenHandler处理，
+   * 然后将解析结果重新拼装成字符串并返回
+   *
+   * @param text 待解析文本
+   * @return 解析后的文档
+   */
   public String parse(String text) {
+    // 校验text是否为空
     if (text == null || text.isEmpty()) {
       return "";
     }
-    // search open token
+    // 查找开始标记
     int start = text.indexOf(openToken);
+    // 校验start是否为-1
     if (start == -1) {
       return text;
     }
     char[] src = text.toCharArray();
     int offset = 0;
+    // 用来记录解析后的字符串
     final StringBuilder builder = new StringBuilder();
+    // 用来记录一个占位符的字面值
     StringBuilder expression = null;
     while (start > -1) {
       if (start > 0 && src[start - 1] == '\\') {
